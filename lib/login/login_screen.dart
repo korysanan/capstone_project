971a10ea114ce28a3d7b67d6f0bd905cda1b2798
korysanan <ujin0561@naver.com>
2024-02-10 20:30:49 +0900

    로그인 및 회원가입 화면 초안

---
+++ lib/login/login_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'sign_up_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'K-Food Box',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 32.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 48.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'EMAIL',
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'PASSWORD',
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 24.0),
+            ElevatedButton(
+              child: Text('Log in'),
+              onPressed: () {
+                print('Login button pressed');
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orange,
+                padding: EdgeInsets.symmetric(vertical: 12.0),
+              ),
+            ),
+            TextButton(
+              child: Text('Forgot Password?'),
+              onPressed: () {
+                print('Forgot Password button pressed');
+              },
+            ),
+            TextButton(
+              child: Text('Need An account? SIGN UP'),
+              onPressed: () {
+                print('SIGN UP button pressed');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SignUpScreen()),
+                );
+              },
+            ),
+            SizedBox(height: 100.0),
+            ElevatedButton(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.person),
+                  SizedBox(width: 8.0),
+                  Text(
+                    'Guest Mode',
+                    style: TextStyle(
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ],
+              ),
+              onPressed: () {
+                print('Guest mode button pressed');
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.grey[400],
+                padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/login/sign_up_screen.dart
@@ -0,0 +1,198 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget {
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  String _selectedLanguage = 'English';
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+  String _passwordError = '';
+  
+  // 비밀번호 유효성 검사
+  bool _validatePassword(String password) {
+    int typesIncluded = 0;
+
+    if (RegExp(r'[A-Z]').hasMatch(password)) typesIncluded++; // 대문자
+    if (RegExp(r'[a-z]').hasMatch(password)) typesIncluded++; // 소문자
+    if (RegExp(r'[0-9]').hasMatch(password)) typesIncluded++; // 숫자
+    if (RegExp(r'[\W]').hasMatch(password)) typesIncluded++; // 특수문자
+
+    return password.length >= 8 && typesIncluded >= 2; // 8이상 조건 2개 이상
+  }
+
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              'K-Food Box',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              'Sign Up',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 32.0),
+            _buildTextFieldWithButton(
+              label: 'Email',
+              buttonText: 'Duplicate check',
+              onPressed: () {
+                print('click Du');
+              },
+              buttonColor: Colors.yellow, 
+            ),
+            _buildTextFieldWithButton(
+              label: 'Certification Number',
+              buttonText: 'certification',
+              onPressed: () {
+                print('click cer');
+              },
+              buttonColor: Colors.blue,
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+                errorText: _passwordError.isNotEmpty ? _passwordError : null, // 비밀번호 검사 결과에 따라 에러 메시지 표시
+              ),
+              obscureText: true,
+              onChanged: (value) {
+                if (!_validatePassword(value)) {
+                  setState(() => _passwordError = 'Invalid password format.');
+                } else {
+                  setState(() => _passwordError = '');
+                }
+              },
+            ),
+            _buildPasswordField('Password check'),
+            SizedBox(height: 8.0),
+            Text(
+              'Password must contain special characters',
+              style: TextStyle(fontSize: 12.0, color: Colors.grey),
+            ),
+            _buildTextFieldWithButton(
+              label: 'Nickname',
+              buttonText: 'Duplicate check',
+              onPressed: () {
+                print('click Du2');
+              },
+              buttonColor: Colors.yellow, 
+            ),
+            _buildDropdownField(),
+            SizedBox(height: 32.0),
+            ElevatedButton(
+              child: Text('Sign up'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orange,
+                padding: EdgeInsets.symmetric(vertical: 12.0),
+              ),
+              onPressed: () {
+                print("sign_up click");
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextFieldWithButton({
+    required String label,
+    required String buttonText,
+    required VoidCallback onPressed,
+    Color buttonColor = Colors.yellow,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: label,
+          border: OutlineInputBorder(),
+          suffixIcon: ElevatedButton(
+            child: Text(buttonText),
+            style: ElevatedButton.styleFrom(
+              primary: buttonColor, // Use the buttonColor passed in as a parameter
+              onPrimary: Colors.black, // Text color for the button
+              textStyle: TextStyle(color: Colors.black),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 16.0),
+            ),
+            onPressed: onPressed,
+          ),
+          suffixIconConstraints: BoxConstraints(
+            minWidth: 0,
+            minHeight: 0,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPasswordField(String label) {
+    bool isPasswordCheckField = label.toLowerCase().contains('check');
+    TextEditingController currentController = isPasswordCheckField ? _confirmPasswordController : _passwordController;
+
+    return TextField(
+      controller: currentController,
+      decoration: InputDecoration(
+        labelText: label,
+        border: OutlineInputBorder(),
+        suffixIcon: isPasswordCheckField
+            ? (_passwordController.text == _confirmPasswordController.text && _confirmPasswordController.text.isNotEmpty
+                ? Icon(Icons.check, color: Colors.green)
+                : SizedBox.shrink())
+            : null,
+      ),
+      obscureText: true,
+      onChanged: isPasswordCheckField ? (_) => setState(() {}) : null,
+    );
+  }
+
+  Widget _buildDropdownField() {
+    return InputDecorator(
+      decoration: InputDecoration(
+        labelText: 'Language',
+        border: OutlineInputBorder(),
+      ),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton<String>(
+          value: _selectedLanguage,
+          items: ['English', 'Korean', 'Chinese', 'Japanese'].map((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+          onChanged: (String? newValue) {
+            if (newValue != null) {
+              setState(() {
+                _selectedLanguage = newValue;
+              });
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
