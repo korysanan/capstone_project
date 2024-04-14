cf253f19395ee5a682490ac16f70d195bc148e3d
korysanan <ujin0561@naver.com>
2024-04-15 01:21:38 +0900

    세션 정보 넣기

--- lib/login/login/login_service.dart
+++ lib/login/login/login_service.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../../home/main_screen.dart';
+import 'login_screen.dart';
 
 class LoginService {
-  // 로그인 서버 쪽
   static Future<void> login(BuildContext context, String email, String password) async {
     var response = await http.post(
       Uri.parse('http://api.kfoodbox.click/login'),
@@ -16,6 +17,14 @@ class LoginService {
     );
 
     if (response.statusCode == 200) {
+      // 세션 ID 저장
+      String? sessionId = response.headers['set-cookie'];
+      if (sessionId != null) {
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        await prefs.setString('sessionId', sessionId);
+      }
+      print("아이디 : $sessionId");
+
       showDialog(
         context: context,
         barrierDismissible: false,
@@ -47,6 +56,48 @@ class LoginService {
           );
         },
       );
+    }
+  }
+
+  static Future<void> logout(BuildContext context) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? sessionId = prefs.getString('sessionId');
+
+    if (sessionId != null) {
+      var response = await http.post(
+        Uri.parse('http://api.kfoodbox.click/logout'), // 실제 로그아웃을 처리할 서버의 URI
+        headers: {
+          'Content-Type': 'application/json',
+          'Cookie': sessionId, // 서버에 세션 ID 전송
+        },
+      );
+
+      if (response.statusCode == 200) {
+        print("성공?");
+        // 서버 응답 성공 시 로컬 세션 ID 삭제
+        await prefs.remove('sessionId');
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => LoginScreen()), // 로그인 화면으로 이동
+        );
+      } else {
+        // 로그아웃 실패 처리, 에러 메시지를 보여주거나 로깅
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Logout Failed'),
+              content: Text('Failed to logout. Please try again.'),
+              actions: <Widget>[
+                TextButton(
+                  child: Text('OK'),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+              ],
+            );
+          },
+        );
+      }
     }
   }
 }
