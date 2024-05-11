e791d0732a7063fb38dba3c63e1b70ab4c534dd3
황보성우 <114071008+sung6770@users.noreply.github.com>
2024-05-11 16:46:08 +0900

    번역 - 정적 정보 추가

--- lib/translate/translation_manager.dart
+++ lib/translate/translation_manager.dart
@@ -1,11 +1,67 @@
-import "language/lang_en.dart";
 import "language/lang_ko.dart";
+import "language/lang_el.dart";
+import "language/lang_nl.dart";
+import "language/lang_no.dart";
+import "language/lang_da.dart";
+import "language/lang_de.dart";
+import "language/lang_lv.dart";
+import "language/lang_ru.dart";
+import "language/lang_ro.dart";
+import "language/lang_lt.dart";
+import "language/lang_bg.dart";
+import "language/lang_sv.dart";
+import "language/lang_es.dart";
+import "language/lang_sk.dart";
+import "language/lang_sl.dart";
+import "language/lang_ar.dart";
+import "language/lang_et.dart";
+import "language/lang_en.dart";
+import "language/lang_eu.dart";
+import "language/lang_uk.dart";
+import "language/lang_it.dart";
+import "language/lang_id.dart";
 import "language/lang_ja.dart";
 import "language/lang_zh.dart";
+import "language/lang_cs.dart";
+import "language/lang_tr.dart";
+import "language/lang_pt.dart";
+import "language/lang_br.dart";
+import "language/lang_pl.dart";
+import "language/lang_fr.dart";
+import "language/lang_fi.dart";
+import "language/lang_hu.dart";
 
 Map<String, Map<String, String>> appTexts = {
-  'en': langEn,
   'ko': langKo,
+  'el': langEl,
+  'nl': langNl,
+  'no': langNo,
+  'da': langDa,
+  'de': langDe,
+  'lv': langLv,
+  'ru': langRu,
+  'ro': langRo,
+  'lt': langLt,
+  'bg': langBg,
+  'sv': langSv,
+  'es': langEs,
+  'sk': langSk,
+  'sl': langSl,
+  'ar': langAr,
+  'et': langEt,
+  'en': langEn,
+  'eu': langEu,
+  'uk': langUk,
+  'it': langIt,
+  'id': langId,
   'ja': langJa,
   'zh': langZh,
+  'cs': langCs,
+  'tr': langTr,
+  'pt': langPt,
+  'br': langBr,
+  'pl': langPl,
+  'fr': langFr,
+  'fi': langFi,
+  'hu': langHu,
 };
