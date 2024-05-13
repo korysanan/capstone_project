2749276d37e37e61b90785b72427e7c7a816a8c6
황보성우 <114071008+sung6770@users.noreply.github.com>
2024-05-14 00:44:13 +0900

    번역 관련 API 키 업데이트 & 구글 번역 API 추가

--- lib/globals.dart
+++ lib/globals.dart
@@ -4,8 +4,10 @@ import 'translate/translation_manager.dart';
 
 String selectedLanguageCode = 'en'; // 기본 언어 코드
 
-final apiKey = '7f0cb90f-8897-474f-a7d0-570e0f6a5bd2:fx';
+// const apiKey = '7f0cb90f-8897-474f-a7d0-570e0f6a5bd2:fx';
+const apiKey = 'cc306a14-243b-4106-8133-633127ed6af8:fx';
 String ODSay_apiKey = "NmHBe2KbysaotzsEo6+3ewb5Dke9KHGzmtW+QDXXvJM";
+const googleTranslateApiKey = 'AIzaSyBq5r5xStMzDC56y1nx0HUoQdhi5JGOwJM';
 
 // 글로벌 함수
 String getText(String key) {
@@ -24,7 +26,7 @@ void updateUserLanguage(String newLanguage) {
 void setLanguageCode() {
   // user_language가 null이 아니고, 숫자로 변환 가능한 경우
   int? languageCode = int.tryParse(user_language ?? '');
-  
+
   if (languageCode != null) {
     switch (languageCode) {
       case 1:
