2f3afe6e6e83e43bc4d94d3a3f23a447e0ff8226
황보성우 <114071008+sung6770@users.noreply.github.com>
2024-05-15 21:49:34 +0900

    .

---
+++ lib/food_restaurant/food_restaurant.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+import 'region_select.dart';
+import 'restaurant_info.dart';
+
+class RestaurantDetailsPage extends StatelessWidget {
+  final int food_id;
+  final String food_name;
+  final Map<String, dynamic> restaurantData;
+
+  const RestaurantDetailsPage(
+      {Key? key,
+      required this.restaurantData,
+      required this.food_id,
+      required this.food_name})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    int restaurantCount = restaurantData['restaurants'].length;
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.push(
+              // 새 페이지로 이동
+              context,
+              MaterialPageRoute(
+                  builder: (context) => RegionSelectScreen(
+                        food_id: food_id,
+                        food_name: food_name,
+                      )),
+            );
+          },
+        ),
+        title: const Text('Select Restaurants'),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: restaurantCount,
+        itemBuilder: (BuildContext context, int index) {
+          var restaurant = restaurantData['restaurants'][index];
+          return InkWell(
+            onTap: () {
+              Navigator.push(
+                // 새 페이지로 이동
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        RestaurantPage(restaurant_info: restaurant)),
+              );
+            },
+            child: Card(
+              elevation: 4.0,
+              margin: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  restaurant['imageUrl'] != null &&
+                          restaurant['imageUrl'].isNotEmpty
+                      ? Image.network(
+                          restaurant['imageUrl'],
+                          width: double.infinity,
+                          height: 200.0,
+                          fit: BoxFit.cover,
+                          errorBuilder: (context, error, stackTrace) {
+                            return const Icon(
+                                Icons.error); // 네트워크 이미지 로딩 실패 시 에러 아이콘 표시
+                          },
+                        )
+                      : Image.asset(
+                          'assets/images/image_comming_soon.png', // 로컬 에셋에서 이미지 로드
+                          width: double.infinity,
+                          height: 200.0,
+                          fit: BoxFit.cover,
+                        ),
+                  ListTile(
+                    title: Text('업체명: ${restaurant['name']}'),
+                    subtitle: restaurant['visitorRating'] != null
+                        ? Text('평점: ${restaurant['visitorRating']}')
+                        : null,
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/food_restaurant/service/restaurant_service.dart
@@ -0,0 +1,27 @@
+// restaurant_service.dart
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+// 서버로부터 데이터를 불러오는 함수
+Future<Map<String, dynamic>?> fetchRestaurantsData(
+    int koreaRegionId, int restaurantCategoryId) async {
+  // API URL 구성
+  Uri url = Uri.parse(
+      'http://api.kfoodbox.click/korea-restaurants?koreaRegionId=$koreaRegionId&restaurantCategoryId=$restaurantCategoryId');
+
+  try {
+    // GET 요청
+    var response = await http.get(url, headers: {'accept': '*/*'});
+
+    if (response.statusCode == 200) {
+      return json.decode(utf8.decode(response.bodyBytes));
+    } else {
+      // 서버 에러 처리
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  } catch (e) {
+    // 네트워크 또는 요청 에러 처리
+    print('An error occurred: $e');
+  }
+  return null;
+}
