4059cdac15d56a717127a47cf2b6fa35089afb77
황보성우 <114071008+sung6770@users.noreply.github.com>
2024-05-27 19:19:45 +0900

    로그인 입력창 키보드에 가림 문제 해결

--- lib/login/screen/login_screen.dart
+++ lib/login/screen/login_screen.dart
@@ -12,123 +12,133 @@ class LoginScreen extends StatelessWidget {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
+  LoginScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      resizeToAvoidBottomInset: false,
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            mainAxisSize: MainAxisSize.min, // Ensures the content takes only needed space
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.asset('assets/images/kfood_logo.png', height: 400), // Logo Image
-              TextField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  hintText: 'Your email',
-                  prefixIcon: Icon(Icons.person),
-                  filled: true,
-                  fillColor: Color(0xFFF1E6FF),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30),
-                    borderSide: BorderSide.none,
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisSize: MainAxisSize
+                  .min, // Ensures the content takes only needed space
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset('assets/images/kfood_logo.png',
+                    height: 400), // Logo Image
+                TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    hintText: 'Your email',
+                    prefixIcon: const Icon(Icons.person),
+                    filled: true,
+                    fillColor: const Color(0xFFF1E6FF),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 10),
-              TextField(
-                controller: passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: 'Your password',
-                  prefixIcon: Icon(Icons.lock),
-                  filled: true,
-                  fillColor: Color(0xFFF1E6FF),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30),
-                    borderSide: BorderSide.none,
+                const SizedBox(height: 10),
+                TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Your password',
+                    prefixIcon: const Icon(Icons.lock),
+                    filled: true,
+                    fillColor: const Color(0xFFF1E6FF),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 60),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color.fromARGB(255, 117, 201, 243), // Button color
-                  minimumSize: Size(double.infinity, 50), // Button size
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                ),
-                onPressed: () {
-                  if (_validateEmail(emailController.text)) {
-                    LoginService.login(context, emailController.text, passwordController.text);
-                  } else {
-                    showDialog(
-                      context: context,
-                      builder: (ctx) => AlertDialog(
-                        title: Text('Invalid Email'),
-                        content: Text('Please enter a valid email address.'),
-                        actions: <Widget>[
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(ctx).pop();
-                            },
-                            child: Text('OK'),
-                          ),
-                        ],
-                      ),
-                    );
-                  }
-                },
-                child: Text('LOGIN'),
-              ),
-              SizedBox(height: 20),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SignUpScreen()),
-                  );
-                },
-                child: Text(
-                  "Don't have an Account ? Sign Up",
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
-              SizedBox(height: 120),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
+                const SizedBox(height: 60),
+                ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Color.fromARGB(255, 249, 195, 255),
-                    minimumSize: Size(double.infinity, 50),
+                    backgroundColor: const Color.fromARGB(
+                        255, 117, 201, 243), // Button color
+                    minimumSize: const Size(double.infinity, 50), // Button size
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(30),
                     ),
                   ),
-                  onPressed: () async {
-                    await fetchRecommendedFoods();
-                    showDialog(
-                      context: context,
-                      barrierDismissible: false,
-                      builder: (BuildContext context) {
-                        return Center(child: CircularProgressIndicator());
-                      },
-                    );
-                    await Future.delayed(Duration(seconds: 1));
-                    Navigator.pop(context);
+                  onPressed: () {
+                    if (_validateEmail(emailController.text)) {
+                      LoginService.login(context, emailController.text,
+                          passwordController.text);
+                    } else {
+                      showDialog(
+                        context: context,
+                        builder: (ctx) => AlertDialog(
+                          title: const Text('Invalid Email'),
+                          content:
+                              const Text('Please enter a valid email address.'),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(ctx).pop();
+                              },
+                              child: const Text('OK'),
+                            ),
+                          ],
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text('LOGIN'),
+                ),
+                const SizedBox(height: 20),
+                GestureDetector(
+                  onTap: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => KFoodBoxHome()),
+                      MaterialPageRoute(builder: (context) => SignUpScreen()),
                     );
                   },
-                  child: Text('GUEST MODE'),
+                  child: const Text(
+                    "Don't have an Account ? Sign Up",
+                    style: TextStyle(color: Colors.black),
+                  ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 120),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 249, 195, 255),
+                      minimumSize: const Size(double.infinity, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    onPressed: () async {
+                      await fetchRecommendedFoods();
+                      showDialog(
+                        context: context,
+                        barrierDismissible: false,
+                        builder: (BuildContext context) {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        },
+                      );
+                      await Future.delayed(const Duration(seconds: 1));
+                      Navigator.pop(context);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => KFoodBoxHome()),
+                      );
+                    },
+                    child: const Text('GUEST MODE'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
